eadfa538af78e994b9e32990a698daae38c15f98
Anaqueen Skywalker <bortoletto222@gmail.com>
2026-06-16 16:46:16 +0100

    main.tf dev altered

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,24 +1,9 @@
-/*module "vpc" {
-  source      = "../../modules/vpc"
-  vpc_cidr    = "10.0.0.0/16"
-  environment = "dev"
-  }
-
-module "ec2" {
-  source        = "../../modules/ec2"
-  ami_id        = ami-0152204c1a187337c
-  instance_type = t3.micro
-  subnet_id     = module.vpc.public_subnet_id
-  environment   = "dev"
-}*/
 locals {
   common_tags = {
     Project   = "my-devops-project"
     ManagedBy = "terraform"
   }
 }
-
-
 
 module "vpc" {
   source      = "../../modules/vpc"
