12102886069c5231fcf7a29f4611a5848dddcc1a
Anaqueen Skywalker <bortoletto222@gmail.com>
2026-06-16 16:52:19 +0100

    test Actions

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,6 @@
+# Triggering GitHub Actions workflow test - Najila
+
+output "github_actions_test" {
+  description = "Test output to trigger CI/CD pipeline"
+  value       = "Workflow triggered successfully"
+}
